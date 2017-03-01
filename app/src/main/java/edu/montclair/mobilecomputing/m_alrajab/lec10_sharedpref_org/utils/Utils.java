780f0678a77d746b8df167cf7173c35525042858
jjmka07 <jjmka09@yahoo.com>
2017-03-01 00:04:41 -0500

    Initial commit

---
+++ app/src/main/java/edu/montclair/mobilecomputing/m_alrajab/lec10_sharedpref_org/utils/Utils.java
@@ -0,0 +1,65 @@
+package edu.montclair.mobilecomputing.m_alrajab.lec10_sharedpref_org.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by m_alrajab on 2/22/17.
+ */
+
+public class Utils {
+
+    public static final String SHARED_PREF_FILENAME="edu.montclair.mobilecomputing.m_alrajab.lec10_sharedpref_org.SHAREDFILE1";
+    public static final String KEY_TITLE="Title_";
+    public static final String KEY_BODY="Body_";
+
+
+    public static String[] getListFromSP(Context context, String key){
+        SharedPreferences sharedPreferences=context.getSharedPreferences(SHARED_PREF_FILENAME,
+                Context.MODE_PRIVATE);
+        Map<String, ?> map=sharedPreferences.getAll();
+        ArrayList<String> lst= new ArrayList<>();
+        for(String str:map.keySet()){
+            if(str.startsWith(key))
+                lst.add((String)map.get(str));
+        }
+        return lst.toArray(new String[lst.size()]);
+    }
+
+    //Gets an array of File names
+    public static String[] getListFromFiles(Context context){
+        ArrayList<String> lstOfFilesInMemeory = new ArrayList<>();
+        try{
+            File filesDir = context.getFilesDir();
+            File[] files = filesDir.listFiles();
+            for(int i=1;i<files.length;i++)
+                lstOfFilesInMemeory.add(files[i].getName().toString());
+        }catch (Exception e){
+            Log.e("ERROR", e.getMessage());
+        }
+        String[] list = lstOfFilesInMemeory.toArray(new String[lstOfFilesInMemeory.size()]);
+        return list;
+    }
+
+    //Gets the text of a file
+    public static String getFileByName(Context context, String filename){
+        String tempStr = "";
+        try{
+            FileInputStream inputtStream = context.openFileInput(filename.replace(" ", ""));
+            int c;
+            while((c=inputtStream.read())!=-1){
+                tempStr+=Character.toString((char) c);
+            }
+            inputtStream.close();
+        }catch (Exception e){
+            Log.e("ERROR", e.getMessage());
+        }
+        return tempStr;
+    }
+}
